27d9391b2eabe2ac446409715608c5f6adec86aa
helberte <helberteads@gmail.com>
2022-07-10 15:58:19 -0400

    Adição da responsividade ao site e menu lateral

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function addClassActiveMenuMobile(){
+  let ul = document.getElementsByClassName("ul-mobile-menu");
+  ul[0].classList.toggle("active");
+
+  document.getElementById("container").classList.toggle("fixed");
+}
+function addClassDesactiveMenuMobile(){
+  let ul = document.getElementsByClassName("ul-mobile-menu");
+  ul[0].classList.toggle("active");
+
+  document.getElementById("container").classList.toggle("fixed");
+}
