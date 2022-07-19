7e9ec290cfa432b8dec4eb609f3b0996ef5b2333
HELBERTE ARRUDA <helberteads@gmail.com>
2022-07-19 08:34:30 -0400

    Adição de animação no amburguer menu

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
 function addClassActiveMenuMobile(){
   let ul = document.getElementsByClassName("ul-mobile-menu");
+  let item1 = document.querySelectorAll('.nav-menu .mobile-menu .item-1')[0];
+  let item3 = document.querySelectorAll('.nav-menu .mobile-menu .item-3')[0];
+  let item2 = document.querySelectorAll('.nav-menu .mobile-menu .item-2')[0];
+
   ul[0].classList.toggle("active");
+  item1.classList.toggle('animation');
+  item3.classList.toggle('animation');
+  item2.classList.toggle('animation');
 
   document.getElementById("container").classList.toggle("fixed");
+
 }
 function addClassDesactiveMenuMobile(){
   let ul = document.getElementsByClassName("ul-mobile-menu");
